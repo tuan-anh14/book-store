6826b6f42b4ea93b9e9f370e18b30e326ab8b6ab
Thieu Tuan Anh <thieutuananh14092k4@gmail.com>
2025-05-21 15:47:18 +0700

    Update design register

--- src/pages/client/auth/register.tsx
+++ src/pages/client/auth/register.tsx
@@ -1,9 +1,15 @@
-import { App, Button, Divider, Form, Input } from 'antd';
+import { App, Button, Divider, Form, Input, Space, theme } from 'antd';
 import type { FormProps } from 'antd';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './register.scss';
 import { registerAPI } from '@/services/api';
+import {
+    UserOutlined,
+    LockOutlined,
+    MailOutlined,
+    PhoneOutlined,
+} from '@ant-design/icons';
 
 type FieldType = {
     fullName: string;
@@ -15,7 +21,8 @@ type FieldType = {
 const RegisterPage = () => {
     const [isSubmit, setIsSubmit] = useState(false);
     const { message } = App.useApp();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const { token } = theme.useToken();
 
     const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
         setIsSubmit(true);
@@ -29,18 +36,14 @@ const RegisterPage = () => {
             );
 
             if (res.data) {
-                //success
                 message.success("Đăng ký user thành công.");
                 navigate("/login");
             } else {
-                //error
                 message.error(res.message)
             }
             setIsSubmit(false)
         }, 2000);
     };
-
-
 
     return (
         <div className='register-page'>
@@ -49,59 +52,74 @@ const RegisterPage = () => {
                     <section className='wrapper'>
                         <div className='heading'>
                             <h2 className='text text-large'>Đăng ký tài khoản</h2>
-                            <Divider />
+                            <p className='text text-normal'>Tạo tài khoản để mua sắm tại BookStore</p>
                         </div>
                         <Form
                             name='form-register'
                             onFinish={onFinish}
                             autoComplete='off'
+                            size='large'
                         >
                             <Form.Item<FieldType>
-                                labelCol={{ span: 24 }}
-                                label="Họ tên"
                                 name="fullName"
                                 rules={[{ required: true, message: 'Họ tên không được để trống!' }]}
                             >
-                                <Input />
+                                <Input
+                                    prefix={<UserOutlined className="prefixIcon" />}
+                                    placeholder="Họ và tên của bạn"
+                                />
                             </Form.Item>
 
                             <Form.Item<FieldType>
-                                labelCol={{ span: 24 }}
-                                label="Email"
                                 name="email"
                                 rules={[
                                     { required: true, message: 'Email không được để trống!' },
                                     { type: "email", message: "Email không đúng định dạng!" }
                                 ]}
                             >
-                                <Input />
+                                <Input
+                                    prefix={<MailOutlined className="prefixIcon" />}
+                                    placeholder="Email của bạn"
+                                />
                             </Form.Item>
 
                             <Form.Item<FieldType>
-                                labelCol={{ span: 24 }}
-                                label="Mật khẩu"
                                 name="password"
                                 rules={[{ required: true, message: 'Mật khẩu không được để trống!' }]}
                             >
-                                <Input.Password />
+                                <Input.Password
+                                    prefix={<LockOutlined className="prefixIcon" />}
+                                    placeholder="Mật khẩu của bạn"
+                                />
                             </Form.Item>
 
                             <Form.Item<FieldType>
-                                labelCol={{ span: 24 }}
-                                label="Số điện thoại"
                                 name="phone"
                                 rules={[{ required: true, message: 'Số điện thoại không được để trống!' }]}
                             >
-                                <Input />
+                                <Input
+                                    prefix={<PhoneOutlined className="prefixIcon" />}
+                                    placeholder="Số điện thoại của bạn"
+                                />
                             </Form.Item>
 
                             <Form.Item>
-                                <Button type='primary' htmlType='submit' loading={isSubmit}>
+                                <Button type='primary' htmlType='submit' loading={isSubmit} block>
                                     Đăng ký
                                 </Button>
                             </Form.Item>
 
-                            <Divider>Or</Divider>
+                            <div className="other-login">
+                                <Divider>Hoặc đăng ký với</Divider>
+                                <Space>
+                                    <Button type="default" icon={<UserOutlined />} block>
+                                        Google
+                                    </Button>
+                                    <Button type="default" icon={<UserOutlined />} block>
+                                        Facebook
+                                    </Button>
+                                </Space>
+                            </div>
 
                             <p className='text text-normal' style={{ textAlign: "center" }}>
                                 Đã có tài khoản?{" "}
